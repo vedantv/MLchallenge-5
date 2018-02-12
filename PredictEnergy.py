85f88f6eb9230e8bd7cbb46af65b0984c139dde2
vedantv <vedant1966@gmail.com>
2018-02-12 22:33:44 +0530

    some data analysis

--- PredictEnergy.py
+++ PredictEnergy.py
@@ -1,6 +1,12 @@
 import pandas as pd
-
+import math
 
 df = pd.read_csv('train.csv')
-
+print(len(df))
+df['avg_T']=(df['T1']+df['T2']+df['T3']+df['T4']+df['T5']+df['T6']+df['T7']+df['T8']+df['T9'])/9
+df['avg_T']=df['avg_T']-df['T_out']
+df['avg_RH']=0
+for i in range(1,10):
+    h='RH_'+str(i)
+    df['avg_RH']+=df[h]
 print(df.head())
